c918098f82b3b19bae6f0ab4bf7c9940fdb52061
Fomin Alexander <inzhener@outlook.com>
2020-09-18 18:18:42 +0300

    создал класс Point

---
+++ Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace snake
+{
+    public class Point
+    {
+        public Point()
+        {
+        }
+
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
