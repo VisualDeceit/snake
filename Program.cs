0e1081d21da54ee0765ca105735c865fd18f8cd9
Fomin Alexander <inzhener@outlook.com>
2020-09-19 11:06:19 +0300

    добавил список List

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace snake
 {
@@ -7,11 +8,24 @@ namespace snake
         static void Main(string[] args)
         {
 
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Point p1 = new Point(1, 2, '*');
 
-            Point p2 = new Point(4, 5, '#') ;
-            p2.Draw();
+            Point p2 = new Point(3, 4, '#') ;
+
+            Point p3 = new Point(5, 6, '$');
+
+            Point p4 = new Point(7, 8, '%');
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
 
 
             Console.ReadLine();
